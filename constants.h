5db804616ee32f92eb000a1765e9c6ec20bb47f9
BlackRoosie <dariasport@vp.pl>
2023-05-25 21:42:35 +0200

    working reverse and lCounter

---
+++ constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#include <stdint.h>
+
+const int KEYBYTES = 16;
+const int NONCEBYTES = 12;  //m
+const int ROUNDS = 80;
+const int NBYTES = 20; //n-bit permutation; for Dumbo n=160
+
+
+#endif 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 
+#include "constants.h"
+#include "spongent.cpp"
+
+#include <bitset>
+
 using namespace std;
 
+void encryption(unsigned char* key, unsigned char*  nonce, unsigned char* ad, unsigned char* message){
+
+
+}
+
+
 int main(){
-    cout<<"Hello elephant"<<endl;
+
+    // unsigned char key[KEYBYTES];
+    // unsigned char nonce[NONCEBYTES];
+
+    unsigned char ad[5] = {'A', 'S', 'C', 'O', 'N'};
+    // unsigned char plain[5] = {'a', 's', 'c', 'o', 'n'};
+
+    // cout<<bitset<8>('e')<<endl;
+    unsigned char trial = reverse('e');
+    // cout<<bitset<8>(trial)<<endl;
+
+    // cout<<bitset<8>(lCounter(0x75));
+
 
 
 
